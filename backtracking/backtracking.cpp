04df58833a7356bfb1eb6ef47d884c1b8964f11a
jackeddaniel <siddhant.ugarkar@gmail.com>
2025-06-24 10:20:48 +0000

    this shit is bs

---
+++ backtracking/backtracking.cpp
@@ -0,0 +1,4 @@
+// backtracking is a problem solving technique where we check all the paths to be taken and then "backtrack" if we reach a deadend.
+// a backtracking solution is usually recursive and must have a definite base case which terminates the recursive function
+// pruning unnecessary paths is also very important
+
