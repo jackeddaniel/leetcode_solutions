eb9cf854df5e7c9c5bd24460034c1f8a9ac75e60
jackeddaniel <siddhant.ugarkar@gmail.com>
2025-07-16 04:33:46 +0000

    need to speed up

--- graphs/944_rotting_oranges.cpp
+++ graphs/944_rotting_oranges.cpp
@@ -1,4 +1,3 @@
-//multi-source bfs
 
 class Solution {
     public: 
@@ -8,29 +7,70 @@ class Solution {
 
             for(int i = 0; i < grid.size(); i++) {
                 for(int j = 0; j < grid[0].size(); j++) {
-                    if(grid[i][j] == 0) continue;
-                    if(grid[i][j] == 1) n++;
-                    if(grid[i][j] == 2) q.push({i,j});
+                    if(grid[i][j] == 2) {
+                        q.push({i,j});
+                    }
+                    else if(grid [i][j] == 1) {
+                        n++;
+                    }
+                    else {
+                        continue;
+                    }
                 }
             }
             if(n == 0) return 0;
-
+            cout<<"initial n: "<<n<<"\n";
             int mins = 0;
+            int rotted = 0;
 
             while(!q.empty()) {
 
                 int size = q.size();
+                rotted = 0;
 
-                for(int i = 0; i < size(); i++) {
-                    int a = q.front().left;
-                    int b = q.front().right;
+                for(int i = 0; i < size; i++) {
+                    int a = q.front().first;
+                    int b = q.front().second;
 
                     q.pop();
                     
-                    if(i<grid.size()-1;i
+                    if(a > 0 && grid[a-1][b] == 1) {
+                        q.push({a-1,b});
+                        grid[a-1][b] = 2;
+                        n--;
+                        cout<<n<<" ";
+                        rotted = 1;
+                    }
+                    if(a < grid.size() - 1 && grid[a+1][b] == 1) {
+                        q.push({a+1,b});
+                        grid[a+1][b] = 2;
+                        n--;
+                        cout<<n<<" ";
+                        rotted = 1;
+                    }
 
-
-
-
+                    if(b > 0 && grid[a][b-1] == 1) {
+                        q.push({a, b-1});
+                        grid[a][b-1] = 2;
+                        n--;
+                        cout<<n<<" ";
+                        rotted = 1;
+                    }
+                    if(b  < grid[0].size() - 1 && grid[a][b+1] == 1) {
+                        q.push({a, b+1});
+                        grid[a][b+1] = 2;
+                        n--;
+                        cout<<n<<" ";
+                        rotted = 1;
+                    }
+                    
+                }
+                cout<<"\n";
+                if(rotted == 1) mins++;
+                cout<<"mins: "<<mins<<"\n";
+            }
+            cout<<"final n: "<<n<<"\n";
+            if(n>0) return -1;
+            return mins;
         }
 };
