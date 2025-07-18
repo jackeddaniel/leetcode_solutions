22e52129d0089c10b3953c8f1f68e6910d69a35a
jackeddaniel <siddhant.ugarkar@gmail.com>
2025-07-18 07:47:11 +0000

    This might be done of my cleanest solns ever

--- graphs/417_pacific_atlantic_water_flow.cpp
+++ graphs/417_pacific_atlantic_water_flow.cpp
@@ -1,56 +1,77 @@
-//We need to see if there is a descending order path to pacific ocean and atlantic ocean
-//what we can do is maybe iterate through the list and run a dfs
+// Basically run two dfs, one to pacific ocean and one two atlantic ocean
+// We need to keep a check grid 
+// We are using versions to avoid re-initialzing the grid again and again to 0 or any other number
+
+
+
 
 
 class Solution {
     public:
+        void pac_dfs(vector<vector<int>>& heights, vector<vector<int>>& check, int version, int x, int y, int& pac) {
+            if(x < 0 || x > heights.size()-1 || y < 0 || y > heights[0].size()-1 || check[x][y] == version) return;
 
-        void dfs(vector<vector<int>>& heights, int x, int y, int& pac, int& atl) {
-            if(x < 0 || x > heights.size()-1 || y < 0 || y > heights[0].size()-1) return;
             if(x == 0 || y == 0) {
                 pac = 1;
+                check[x][y] = version;
                 return;
             }
-            if(x == heights.size()-1 || y == heights[0].size()-1) {
+
+            check[x][y] = version;
+            if(x > 0 && heights[x-1][y] <= heights[x][y]) pac_dfs(heights,check,version,x-1,y,pac);
+            if(x < heights.size()-1 && heights[x+1][y] <= heights[x][y]) pac_dfs(heights,check,version,x+1,y,pac);
+            if(y > 0 && heights[x][y-1] <= heights[x][y]) pac_dfs(heights,check,version,x,y-1,pac);
+            if(y < heights[0].size()-1 && heights[x][y+1] <= heights[x][y]) pac_dfs(heights,check,version,x,y+1,pac);
+            
+            return;
+        }
+            
+        void atl_dfs(vector<vector<int>>& heights, vector<vector<int>>& check, int version, int x, int y, int& atl) {
+            if (x < 0 || x >= heights.size() || y < 0 || y >= heights[0].size() || check[x][y] == version) return;
+
+            if (x == heights.size() - 1 || y == heights[0].size() - 1) {
                 atl = 1;
+                check[x][y] = version;
                 return;
             }
-    
-            if(heights[x][y] >= heights[x-1][y]) dfs(heights,x-1,y,pac,atl);
-            if(heights[x][y] >= heights[x+1][y]) dfs(heights,x+1,y,pac,atl);
-            if(heights[x][y] >= heights[x][y-1]) dfs(heights,x,y-1,pac,atl);
-            if(heights[x][y] >= heights[x][y+1]) dfs(heights,x,y+1,pac,atl);
+
+            check[x][y] = version;
+
+            if (x > 0 && heights[x-1][y] <= heights[x][y]) atl_dfs(heights, check, version, x-1, y, atl);
+            if (x < heights.size() - 1 && heights[x+1][y] <= heights[x][y]) atl_dfs(heights, check, version, x+1, y, atl);
+            if (y > 0 && heights[x][y-1] <= heights[x][y]) atl_dfs(heights, check, version, x, y-1, atl);
+            if (y < heights[0].size() - 1 && heights[x][y+1] <= heights[x][y]) atl_dfs(heights, check, version, x, y+1, atl);
 
             return;
         }
 
         vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-            int pac;
-            int atl;
             int m = heights.size();
             int n = heights[0].size();
-            vector<vector<int>> check(m,vector<int>(n,0));
+
+            int pac;
+            int atl;
+            int version = 1;
+
+            vector<vector<int>> check(m, vector<int>(n, 0));
+
             vector<vector<int>> res;
-            vector<int> tmp;
             for(int i = 0; i < heights.size(); i++) {
                 for(int j = 0; j < heights[0].size(); j++) {
                     pac = 0;
                     atl = 0;
-                    dfs(heights,i,j,pac,atl);
-                    if(i == 0 && j == 4) {
-                        cout<<pac<< " "<<atl<<"\n";
-                    }
+
+                    version++;
+                    pac_dfs(heights,check,version,i,j,pac);
+
+                    version++;
+                    atl_dfs(heights,check,version,i,j,atl);
+
                     if(pac == 1 && atl == 1) {
-                        tmp.push_back(i);
-                        tmp.push_back(j);
-                        res.push_back(tmp);
-                        tmp.pop_back();
-                        tmp.pop_back();
+                        res.push_back({i,j});
                     }
                 }
-                
             }
             return res;
-
         }
 };
