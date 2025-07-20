938941970d4ada73669db390f2d4e55b52a8ecde
jackeddaniel <siddhant.ugarkar@gmail.com>
2025-07-20 10:02:32 +0000

    okay

--- graphs/130_surrounded_regions.cpp
+++ graphs/130_surrounded_regions.cpp
@@ -1,36 +1,53 @@
 class Solution {
     public:
-        void dfs(vector<vector<char>>& board, vector<vector<int>>& check, int x, int y) {
-            if(x < 0 || x > board.size()-1 || y < 0 || y > board[0].size()-1 || board[x][y] == 'X' || check[x][y] == 1) return;
-
-            check[x][y] = 1;
-
-            if(x > 0) dfs(board,check,x,y);
-            if(x < 
-
+        void dfs(vector<vector<char>>& board, vector<vector<char>>& grid, int x, int y) {
+            if(x < 0 || x > board.size()-1 || y < 0 || y > board[0].size()-1 || grid[x][y] == 'O') {
+                cout<<"returning from dfs"<<"\n";
+                return;
+            }
+            grid[x][y] = 'O';
+            
+            if(x > 0 && board[x-1][y] == 'O') {
+                cout<<"going up"<<"\n";
+                dfs(board,grid, x-1,y);
+            }
+            if(x < board.size()-1 && board[x+1][y] == 'O') {
+                cout<<"going down"<<"\n";
+                dfs(board,grid,x+1,y);
+            }
+            if(y > 0 && board[x][y-1] == 'O') {
+                cout<<"going  left"<<"\n";
+                dfs(board,grid,x,y-1);
+            }
+            if(y < board[0].size()-1 && board[x][y+1] == 'O') {
+                cout<<"going right"<<"\n";
+                dfs(board,grid,x,y+1);
+            }
+        }
 
         void solve(vector<vector<char>>& board) {
             int m = board.size();
             int n = board[0].size();
 
-            vector<vector<int>> check(m, vector<int>(n, 0));
+            vector<vector<char>> grid(m, vector<char>(n, 'X'));
 
-            for(int i = 0; i < board.size(); i++) {
-                for(int j = 0; j < board[0].size(); j++) {
-                    if(i != 0 || i != m-1 || j != 0 || j != n-1 || board[i][j] == 'X') continue;
-
-                    dfs(board, check, i, j);
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if((i == 0 || i == m-1 || j == 0 || j == n-1) && board[i][j] == 'O') {
+                        cout<<"called dfs"<<"\n";
+                        dfs(board,grid,i,j);
+                        
+                    }
+                    cout<<i<<" "<<j<<"\n";
+                    cout<<"did not call dfs"<<"\n";
                 }
             }
-            
-            
-            for(int i = 0; i < board.size(); i++) {
-                for(int j = 0; j < board[0].size(); j++) {
 
-                    if(board[i][j] == '0' && check[i][j] == 0) {
-                        board[i][j] = 'X';
-                    }
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(board[i][j] == 'O' && grid[i][j] == 'X') board[i][j] = 'X';
                 }
             }
         }
 };
+
